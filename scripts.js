ec0a0761b5f5798a95ad6761a8a916f649591e0d
user <user@comlab-05.molloy.edu>
2020-02-10 11:57:31 -0500

    made scripts.js interactive

--- scripts.js
+++ scripts.js
@@ -1,1 +1,7 @@
-a$("#response").html("scripts.js has loaded!");
+let userInput;
+userInput = prompt("What do you want to have for dinner?", "Type your answer here.");
+if (userInput === "burrito") {
+  $("#responce").html("Brilliant choice!");
+} else {
+  $("#responce").html("Don't you want a burrito?");
+}
