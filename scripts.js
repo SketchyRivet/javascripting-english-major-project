0c2f0a6a451ed2e843cccc141709804b5292d406
user <user@comlab-06.molloy.edu>
2020-02-24 11:57:22 -0500

    added prompt to tip calculator

--- scripts.js
+++ scripts.js
@@ -1,7 +1,34 @@
-let userInput;
+/*let userInput;
 userInput = prompt("What do you want to have for dinner?", "Type your answer here.");
 if (userInput === "burrito") {
   $("#response").html("Brilliant choice!");
 } else {
   $("#response").html("Don't you want a burrito?");
 }
+*/
+let tipCalculator;
+tipCalculator = function(multiVariable, divVariable){
+let multiTotal;
+let divTotal;
+if (tipAmount < 1){
+multiTotal = total * tipAmount;
+$("#response").html("Your tip is $" + multiTotal);} else {
+divTotal = total % tipAmount;
+$("#response").html("Your tip is $" + divTotal);
+}
+};
+let tipAmount;
+tipAmount = prompt("What percentage do you want the tip to be?", "Type your answer here.");
+let total;
+total = prompt("What is the bill's total?", "Type your answer here.");
+tipCalculator();
+/*let makeABurrito;
+makeABurrito = function(beansVariable){
+  let beansResponse;
+  beansResponse = "You ordered " + beansVariable + " beans. Good choice!";
+  $("#response").html(beansResponse);
+}
+let blackBeans;
+blackBeans = "black";
+makeABurrito(blackBeans);
+*/
