917c1b20e8ba776115639c843382a1b2613e467a
user <user@comlab-06.molloy.edu>
2020-03-02 11:58:16 -0500

    Added array and methods

--- scripts.js
+++ scripts.js
@@ -6,7 +6,7 @@ if (userInput === "burrito") {
   $("#response").html("Don't you want a burrito?");
 }
 */
-let tipCalculator;
+/*let tipCalculator;
 tipCalculator = function(multiVariable, divVariable){
 let multiTotal;
 let divTotal;
@@ -21,7 +21,7 @@ let tipAmount;
 tipAmount = prompt("What percentage do you want the tip to be?", "Type your answer here.");
 let total;
 total = prompt("What is the bill's total?", "Type your answer here.");
-tipCalculator();
+tipCalculator();*/
 /*let makeABurrito;
 makeABurrito = function(beansVariable){
   let beansResponse;
@@ -32,3 +32,33 @@ let blackBeans;
 blackBeans = "black";
 makeABurrito(blackBeans);
 */
+/*let arrayOfStrings, arrayOfNumbers, arrayMixed;
+arrayOfStrings = ["a", "b", "c"];
+arrayOfNumbers = [1, 2, 3];
+arrayMixed = ["a", 1, null, true, arrayOfNumbers, [4.5, 5.6]];
+$("#response").html(arrayMixed[4]);
+*/
+let myHabaneroSauceSquirts, myBurritoObject;
+// First, define and assign a variable for how
+// spicy the burrito is.
+myHabaneroSauceSquirts = 3;
+// Now assign the burrito object.
+myBurritoObject = {
+tortilla: "wheat",
+guacamole: true,
+beans: "pinto",
+// Make use of the variable above.
+habaneroSauceSquirts: myHabaneroSauceSquirts,
+// Use the variable again in a function.
+spiciness: function(){
+if (myHabaneroSauceSquirts > 0 ){
+alert("This is a spicy burrito!");
+} else {
+alert("This is a mild burrito.");
+}
+}
+};
+$("#response").html("Your " + myBurritoObject.tortilla + " burrito has " +
+myBurritoObject.habaneroSauceSquirts +
+" squirts of habanero.");
+myBurritoObject.spiciness();
